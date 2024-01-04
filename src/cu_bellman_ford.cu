a7cc0fc9d4d810a4bf482a976e785ba9f1e619c0
diaconuccalin <diaconuccalin@gmail.com>
2024-01-04 17:36:24 +0200

    Small improvements

--- src/cu_bellman_ford.cu
+++ src/cu_bellman_ford.cu
@@ -9,6 +9,14 @@ extern "C" {
 }
 
 
+void error_handling() {
+    cudaError_t error = cudaGetLastError();
+
+    if (error != cudaSuccess)
+        printf("%s\n", cudaGetErrorString(error));
+}
+
+
 void cu_initialize_single_source(graph *G, node *s) {
     for (int i = 0; i < G->n; i++) {
         G->nodes[i].d = INT_MAX;
@@ -52,9 +60,11 @@ bool cu_bellman_ford(graph *G, node *s) {
 
     cudaMalloc((void **) &d_n, sizeof(int));
     cudaMalloc((void **) &d_m, sizeof(int));
+    error_handling();
 
     cudaMemcpy(d_n, &G->n, sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_m, &G->m, sizeof(int), cudaMemcpyHostToDevice);
+    error_handling();
 
     // Nodes information
     int *nodes_ds, *nodes_pis;
@@ -62,9 +72,12 @@ bool cu_bellman_ford(graph *G, node *s) {
 
     cudaMalloc((void **) &d_nodes_ds, G->n * sizeof(int));
     cudaMalloc((void **) &d_nodes_pis, G->n * sizeof(int));
+    error_handling();
 
-    nodes_ds = (int *) malloc(G->n * sizeof(int));
-    nodes_pis = (int *) malloc(G->n * sizeof(int));
+    cudaMallocHost((void **) &nodes_ds, G->n * sizeof(int));
+    cudaMallocHost((void **) &nodes_pis, G->n * sizeof(int));
+    error_handling();
+
     for (int i = 0; i < G->n; i++) {
         nodes_ds[i] = G->nodes[i].d;
         nodes_pis[i] = -1;
@@ -72,6 +85,7 @@ bool cu_bellman_ford(graph *G, node *s) {
 
     cudaMemcpy(d_nodes_ds, nodes_ds, G->n * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_nodes_pis, nodes_pis, G->n * sizeof(int), cudaMemcpyHostToDevice);
+    error_handling();
 
     // Edges information
     int *edges_sources, *edges_destinations, *edges_weights;
@@ -80,10 +94,13 @@ bool cu_bellman_ford(graph *G, node *s) {
     cudaMalloc((void **) &d_edges_sources, G->m * sizeof(int));
     cudaMalloc((void **) &d_edges_destinations, G->m * sizeof(int));
     cudaMalloc((void **) &d_edges_weights, G->m * sizeof(int));
+    error_handling();
 
-    edges_sources = (int *) malloc(G->m * sizeof(int));
-    edges_destinations = (int *) malloc(G->m * sizeof(int));
-    edges_weights = (int *) malloc(G->m * sizeof(int));
+    cudaMallocHost((void **) &edges_sources, G->m * sizeof(int));
+    cudaMallocHost((void **) &edges_destinations, G->m * sizeof(int));
+    cudaMallocHost((void **) &edges_weights, G->m * sizeof(int));
+    error_handling();
+
     for (int i = 0; i < G->m; i++) {
         edges_sources[i] = find_node_id_by_name(G->edges[i].source->name, G->nodes, G->m);
         edges_destinations[i] = find_node_id_by_name(G->edges[i].destination->name, G->nodes, G->m);
@@ -93,15 +110,16 @@ bool cu_bellman_ford(graph *G, node *s) {
     cudaMemcpy(d_edges_sources, edges_sources, G->m * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_edges_destinations, edges_destinations, G->m * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_edges_weights, edges_weights, G->m * sizeof(int), cudaMemcpyHostToDevice);
+    error_handling();
 
     cudaDeviceSynchronize();
 
     double end_time = get_time();
-    printf("Memory allocation time: %fs.\n", end_time - start_time);
+    printf("\nMemory allocation time: %fs.\n", end_time - start_time);
 
     // Processing
-    int grid_dim = int(sqrt(G->m));
-    int block_dim = (G->m / grid_dim + 1);
+    dim3 grid_dim(int(sqrt(G->m)));
+    dim3 block_dim(G->m / grid_dim.x + 1);
 
     start_time = get_time();
     for (int i = 1; i < G->n; i++) {
@@ -113,6 +131,7 @@ bool cu_bellman_ford(graph *G, node *s) {
                 d_edges_destinations,
                 d_edges_weights
         );
+        error_handling();
         cudaDeviceSynchronize();
     }
     end_time = get_time();
@@ -120,6 +139,7 @@ bool cu_bellman_ford(graph *G, node *s) {
 
     cudaMemcpy(nodes_ds, d_nodes_ds, G->n * sizeof(int), cudaMemcpyDeviceToHost);
     cudaMemcpy(nodes_pis, d_nodes_pis, G->n * sizeof(int), cudaMemcpyDeviceToHost);
+    error_handling();
     cudaDeviceSynchronize();
 
     for (int i = 0; i < G->n; i++) {
@@ -134,13 +154,15 @@ bool cu_bellman_ford(graph *G, node *s) {
     cudaFree(d_edges_sources);
     cudaFree(d_edges_destinations);
     cudaFree(d_edges_weights);
+    error_handling();
     cudaDeviceSynchronize();
 
-    free(nodes_ds);
-    free(nodes_pis);
-    free(edges_sources);
-    free(edges_destinations);
-    free(edges_weights);
+    cudaFreeHost(nodes_ds);
+    cudaFreeHost(nodes_pis);
+    cudaFreeHost(edges_sources);
+    cudaFreeHost(edges_destinations);
+    cudaFreeHost(edges_weights);
+    error_handling();
 
     for (int i = 0; i < G->m; i++) {
         edge *current_edge = &G->edges[i];
