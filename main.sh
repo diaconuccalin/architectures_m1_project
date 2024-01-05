fd1b374badd60cfa90d635055dd7c5bbf6d96b5a
diaconuccalin <diaconuccalin@gmail.com>
2024-01-05 18:08:03 +0200

    Changed directory for binaries

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-gcc -std=c99 -Wall -Wpedantic -fopenmp src/tester.c src/utils.c  src/graph_file_reader.c src/bellman_ford.c main.c -o main_omp -lgomp
-./main_omp
+gcc -std=c99 -Wall -Wpedantic -fopenmp src/tester.c src/utils.c  src/graph_file_reader.c src/bellman_ford.c main.c -o bin/main_omp -lgomp
+./bin/main_omp
 
-nvcc main.c src/tester.c src/graph_file_reader.c src/cu_bellman_ford.cu src/bellman_ford.c src/utils.c src/timing.c -o main_cuda
-./main_cuda
+nvcc main.c src/tester.c src/graph_file_reader.c src/cu_bellman_ford.cu src/bellman_ford.c src/utils.c src/timing.c -o bin/main_cuda
+./bin/main_cuda
